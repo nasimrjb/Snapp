05b9fd6a067402f4a466266b3d641f111be9175b
Nasim Rajabi <nasim.rjb@gmail.com>
2026-02-13 15:54:34 +0330

    cleaning up

--- Cancellation/McDonalds-Data.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 19 11:28:34 2026
-
-@author: Snapp
-"""
-import pandas as pd
--- Cancellation/untitled0.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 21 21:45:06 2026
-
-@author: Snapp
-"""
-
